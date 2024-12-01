c7b325fc3680d389607868ac3efbb0a974e9f323
Spuriosity1 <alaric.sanders@gmail.com>
2024-12-01 18:41:19 +0000

    ED kinda-sorta working??

--- scripts/pyro_generic.py
+++ scripts/pyro_generic.py
@@ -3,73 +3,74 @@ import numpy as np  # generic math functions
 from quspin.operators import quantum_LinearOperator, hamiltonian
 from quspin.tools import lanczos
 import pyrochlore
+from pyro_ham_construct import is_identity, perm_order, construct_hamiltonian_spec, get_symmetries
 import lattice
 from sympy import Matrix
 import itertools
 import json
 import sys
 
-def is_identity(p: list):
-    return all(x == j for (j, x) in enumerate(p))
-
-
-def perm_order(p: list):
-    order = 1
-    tmp = [x for x in p]
-    while not is_identity(tmp):
-        tmp = [p[x] for x in tmp]
-        order += 1
-        if order > len(p):
-            raise Exception("perm is broken")
-    return order
-
-
-def construct_hamiltonian_spec(lat: lattice.Lattice, Jzz, Jpm, B):
-    Ising_opspec = []
-    xc_opspec = []
-
-    for bond in lat.bonds:
-        Ising_opspec.append([Jzz, bond['from_idx'], bond['to_idx']])
-        xc_opspec.append([Jpm, bond['from_idx'], bond['to_idx']])
-    # gx_opspec = [[g, i] for i in range(L)]
-
-    return dict(
-        static=[["zz", Ising_opspec], ["+-", xc_opspec], ["-+", xc_opspec]],
-        lat=lat
-    )
-
-
-def get_symmetries(lat, strip_trivial=False):
-    T1, T2, T3 = lat.get_transl_generators()
-    I = lat.get_inversion_perm([0, 0, 0])
-
-    syms = {
-        "T1": T1,
-        "T2": T2,
-        "T3": T3,
-        "I": I,
-        'P01': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
-                                 direction=Matrix([0, 1, 1])),
-        'P02': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
-                                 direction=Matrix([1, 0, 1])),
-        'P03': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
-                                 direction=Matrix([1, 1, 0])),
-        'P12': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
-                                 direction=Matrix([0, 1, -1])),
-        'P23': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
-                                 direction=Matrix([0, 1, -1])),
-        'P31': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
-                                      direction=Matrix([0, 1, -1])),
-    }
-
-    if not strip_trivial:
-        return syms
-
-    nontriv_syms = {}
-    for s in syms:
-        if not is_identity(syms[s]):
-            nontriv_syms[s] = syms[s]
-    return nontriv_syms
+#def is_identity(p: list):
+#    return all(x == j for (j, x) in enumerate(p))
+#
+#
+#def perm_order(p: list):
+#    order = 1
+#    tmp = [x for x in p]
+#    while not is_identity(tmp):
+#        tmp = [p[x] for x in tmp]
+#        order += 1
+#        if order > len(p):
+#            raise Exception("perm is broken")
+#    return order
+#
+#
+#def construct_hamiltonian_spec(lat: lattice.Lattice, Jzz, Jpm, B):
+#    Ising_opspec = []
+#    xc_opspec = []
+#
+#    for bond in lat.bonds:
+#        Ising_opspec.append([Jzz, bond['from_idx'], bond['to_idx']])
+#        xc_opspec.append([Jpm, bond['from_idx'], bond['to_idx']])
+#    # gx_opspec = [[g, i] for i in range(L)]
+#
+#    return dict(
+#        static=[["zz", Ising_opspec], ["+-", xc_opspec], ["-+", xc_opspec]],
+#        lat=lat
+#    )
+#
+#
+#def get_symmetries(lat, strip_trivial=False):
+#    T1, T2, T3 = lat.get_transl_generators()
+#    I = lat.get_inversion_perm([0, 0, 0])
+#
+#    syms = {
+#        "T1": T1,
+#        "T2": T2,
+#        "T3": T3,
+#        "I": I,
+#        'P01': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
+#                                 direction=Matrix([0, 1, 1])),
+#        'P02': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
+#                                 direction=Matrix([1, 0, 1])),
+#        'P03': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
+#                                 direction=Matrix([1, 1, 0])),
+#        'P12': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
+#                                 direction=Matrix([0, 1, -1])),
+#        'P23': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
+#                                 direction=Matrix([0, 1, -1])),
+#        'P31': lat.get_refl_perm(origin=Matrix([1, 1, 1]),
+#                                      direction=Matrix([0, 1, -1])),
+#    }
+#
+#    if not strip_trivial:
+#        return syms
+#
+#    nontriv_syms = {}
+#    for s in syms:
+#        if not is_identity(syms[s]):
+#            nontriv_syms[s] = syms[s]
+#    return nontriv_syms
 
 
 def calc_hamiltonian(hamspec, silent=False, **kwargs):
@@ -154,7 +155,8 @@ coupling_consts = dict(
 lattice_symmetries = get_symmetries(full_lat)
 print(lattice_symmetries)
 chosen_symmetries = {k: lattice_symmetries[k]
-                     for k in ['T1', 'T2', 'T3', 'I', 'P01']}
+                     #for k in ['T1', 'T2', 'T3', 'I', 'P01']}
+                     for k in ['T1', 'T2', 'T3']}
 
 
 # SYMMETRY AWARE DIAGONALISATION
